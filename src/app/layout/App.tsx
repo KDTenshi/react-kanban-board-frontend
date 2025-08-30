2c14addc50803fc326f1b0c5438e151d8799c779
Артур Мыльников <kuraidatenshidesu@gmail.com>
2025-08-30 14:46:23 +0300

    Set up Scss for Vite and added Header component

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,7 +1,15 @@
 import type { FC } from "react";
 
+import "../style/App.scss";
+
+import { Header } from "../../features/ui/components/Header";
+
 const App: FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
