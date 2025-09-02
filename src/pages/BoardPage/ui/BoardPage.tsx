d312ebdb8e6e04017b9a3c31755d443b644ce3c8
Артур Мыльников <kuraidatenshidesu@gmail.com>
2025-09-02 13:44:20 +0300

    Added Board Page layout template

---
+++ src/pages/BoardPage/ui/BoardPage.tsx
@@ -0,0 +1,83 @@
+import type { FC } from "react";
+
+import style from "./BoardPage.module.scss";
+
+const BoardPage: FC = () => {
+  return (
+    <div className={style.Page}>
+      <div className={style.Heading}>
+        <div className={style.Menu}>
+          <div className={style.Info}>
+            <div className={style.Title}>
+              <span className={style.Icon}>
+                <span className="material-symbols-outlined">view_kanban</span>
+              </span>
+              <h3 className={style.Name}>Board name</h3>
+            </div>
+            <button className={style.Button}>
+              <span className="material-symbols-outlined">settings</span>
+              Settings
+            </button>
+          </div>
+          <div className={style.Navigation}>
+            <button className={style.Button}>
+              <span className="material-symbols-outlined">summarize</span>
+              About
+            </button>
+            <button className={style.Button}>
+              <span className="material-symbols-outlined">list_alt</span>
+              List
+            </button>
+            <button className={style.Button}>
+              <span className="material-symbols-outlined">view_kanban</span>
+              Board
+            </button>
+          </div>
+        </div>
+        <div className={style.Controls}>
+          <button className={style.Button}>
+            <span className="material-symbols-outlined">view_kanban</span>
+            Add new task
+          </button>
+          <div className={style.Filters}>
+            <button className={style.Button}>
+              <span className="material-symbols-outlined">sort</span>
+              Sort by
+              <span className="material-symbols-outlined">keyboard_arrow_down</span>
+            </button>
+            <button className={style.Button}>
+              <span className="material-symbols-outlined">filter_list</span>
+              Filter by
+              <span className="material-symbols-outlined">keyboard_arrow_down</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={style.Columns}>
+        <div className={style.Column}>
+          <div className={style.Todo}>
+            <span className={style.Dot}></span>
+            <p className={style.Text}>To Do</p>
+          </div>
+          <div className={style.Body}></div>
+        </div>
+        <div className={style.Column}>
+          <div className={style.InProgress}>
+            <span className={style.Dot}></span>
+            <p className={style.Text}>In Progress</p>
+          </div>
+          <div className={style.Body}></div>
+        </div>
+        <div className={style.Column}>
+          <div className={style.Done}>
+            <span className={style.Dot}></span>
+            <p className={style.Text}>Done</p>
+          </div>
+          <div className={style.Body}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BoardPage;
