d2b4ebac39741297748b97428d7e45008e780afd
Артур Мыльников <kuraidatenshidesu@gmail.com>
2025-09-03 16:56:18 +0300

    Added and set up React router

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -4,9 +4,10 @@ import "../style/App.scss";
 
 import { Header } from "../../features/ui/components/Header";
 import { Sidebar } from "../../features/ui/components/Sidebar";
-// import { HomePage } from "../../pages/HomePage";
-// import { BoardsPage } from "../../pages/BoardsPage";
+import { HomePage } from "../../pages/HomePage";
+import { BoardsPage } from "../../pages/BoardsPage";
 import { BoardPage } from "../../pages/BoardPage";
+import { Route, Routes } from "react-router";
 
 const App: FC = () => {
   return (
@@ -14,9 +15,12 @@ const App: FC = () => {
       <Header />
       <div className="Body">
         <Sidebar />
-        {/* <HomePage /> */}
-        {/* <BoardsPage /> */}
-        <BoardPage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/settings" element={<div>Settings</div>} />
+          <Route path="/boards" element={<BoardsPage />} />
+          <Route path="/board" element={<BoardPage />} />
+        </Routes>
       </div>
     </div>
   );
